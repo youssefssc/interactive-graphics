16dac7c29dfe886581c513adae676f623d5bc374
Youssef Faltas <yfaltas@securityscorecard.io>
2021-07-01 17:48:25 +0200

    fix: add min bar height and expand list

--- src/diagrams/ZoomableStack/index.jsx
+++ src/diagrams/ZoomableStack/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect, useRef } from 'react';
 
 import { useSpring, animated } from 'react-spring';
@@ -6,37 +7,86 @@ import { mockData } from './mockData';
 
 import './stack.css';
 
-const STACK_HEIGHT = 500;
+const STACK_HEIGHT = 204;
+const MIN_BAR_HEIGHT = 10;
+const MAX_BAR_HEIGHT = 180;
+const data= mockData;
 
-const ZoomableStack = () => {
-  const [visibleStack, setVisibleStack] = useState(mockData);
+const ZoomableStack = () => {  
+ const initalHeights = Object.fromEntries(
+    Object.entries(data).map(([key]) => [key, 0]),
+  );
+
+  const [visibleStack, setVisibleStack] = useState(data);
+  const [barHeights, setBarHeights] = useState(initalHeights);
   const [zoomLevel, setZoomLevel] = useState(100);
+  const [areBarsExpanded, setAreBarsExpanded] = useState(false);
 
-  const minData = Math.min(...Object.values(mockData));
-  const maxData = Math.max(...Object.values(mockData));
+  useEffect(() => {
+    setVisibleStack(data);
+    setZoomLevel(100);
+    calculateBarHeights(data);
+  }, [data]);
 
-  const zoomToLevel = event => {
-    const level = event.target.value;
-    setZoomLevel(level);
+  useEffect(() => {
+    zoomToLevel(zoomLevel);
+    if (zoomLevel > 50) {
+      setAreBarsExpanded(false);
+    }
+  }, [zoomLevel]);
+
+  useEffect(() => {
+    if (zoomLevel <= 50) {
+      calculateBarHeights(visibleStack);
+    }
+  }, [areBarsExpanded]);
+
+  // const total = Object.values(data).reduce((acc, value) => acc + value, 0);
+  const minData = Math.min(...Object.values(data));
+  const maxData = Math.max(...Object.values(data));
+  // const meanData = (minData + maxData) / 2;
+
+  const zoomToLevel = level => {
     const zoomValue = (level / 100) * maxData + minData;
-    const zoomObj = Object.fromEntries(
-      Object.entries(mockData).filter(([key, value]) => value < zoomValue)
+    const zoomObject = Object.fromEntries(
+      Object.entries(data).filter(([, value]) => value < zoomValue),
     );
-    setVisibleStack(zoomObj);
-    return zoomObj;
+    setVisibleStack(zoomObject);
+    calculateBarHeights(zoomObject);
+    return zoomObject;
   };
 
-  const reMapHeight = (key, value) => {
-    let height = 0;
-    if (Object.keys(visibleStack).includes(key)) {
-      height = 100;
-    }
+  const calculateBarHeights = barsObject => {
+    setBarHeights(
+      Object.fromEntries(
+        Object.entries(barsObject).map(([key, value]) => [
+          key,
+          remapHeight(key, value),
+        ]),
+      ),
+    );
+  };
+
+  const remapHeight = (key, value) => {
+    let height = MIN_BAR_HEIGHT;
 
     const visibleValues = Object.values(visibleStack);
     const findSum = (accumulator, currentValue) => accumulator + currentValue;
-
     const sumVisibleValues = visibleValues.reduce(findSum);
+
     height = (value / sumVisibleValues) * STACK_HEIGHT;
+
+    if (
+      areBarsExpanded &&
+      Object.keys(visibleStack).includes(key) &&
+      height < 3 * MIN_BAR_HEIGHT
+    ) {
+      height = 3 * MIN_BAR_HEIGHT;
+    }
+
+    if (height > MAX_BAR_HEIGHT) {
+      height = MAX_BAR_HEIGHT;
+    }
 
     return height;
   };
@@ -51,14 +101,20 @@ const ZoomableStack = () => {
           step='1'
           min='1'
           max='100'
-          value={zoomLevel}
-          onChange={zoomToLevel}
+         value={zoomLevel}
+        onChange={event => setZoomLevel(Number(event.target.value))}
         />
         <p>
           zoom level
           <br />
           {100 - zoomLevel}
         </p>
+        <button
+           onClick={() => setAreBarsExpanded(!areBarsExpanded)}
+            disabled={zoomLevel > 50}
+          >
+            {areBarsExpanded ? 'Collpase' : 'Expand'}
+        </button>
       </div>
       <div className='stack'>
         {Object.entries(mockData).map(([key, value], index) => (
@@ -67,7 +123,7 @@ const ZoomableStack = () => {
             index={index}
             value={value}
             title={key}
-            height={reMapHeight(key, value)}
+            height={barHeights[key]}
             isShowing={Object.keys(visibleStack).includes(key)}
           />
         ))}
@@ -87,16 +143,17 @@ const AnimatedBar = ({ title, value, index, height, isShowing }) => {
     config: {
       duration: 300,
     },
-    opacity: isShowing ? 1 : 0,
+   opacity: isShowing ? 1 : 0,
     height: isShowing ? height : 0,
+    borderColor: height > 5 ? '#FFFFFF' : '#5D5D5D',
     backgroundColor:
-      height < 30 ? '#333' : index % 2 === 0 ? '#abcccf' : '#f1faeb',
-    width: 400,
+      height < 30 ? '#5D5D5D' : index % 2 === 0 ? '#CDD6E2' : '#B1C3DB',
+    width: 306,
   });
 
   return (
     <animated.div className='box' style={props}>
-      {height > 30 && (
+      {height >= 30 && (
         <span>
           <b>{title}</b> {value}
         </span>
